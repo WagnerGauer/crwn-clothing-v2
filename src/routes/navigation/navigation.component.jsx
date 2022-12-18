8aa695f9a8361890e444aba382becc7b435083f1
Wagner Gauer <81208157+WagnerGauer@users.noreply.github.com>
2022-12-18 16:53:47 -0300

    move navigation logic to a new file, use the react Link tag to move the user to different pages

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,22 @@
+import { Fragment } from "react"; // react forces me to use a top level element, if I don't want to have a div in my html, I have to use this
+import { Outlet, Link } from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to="/">
+                    <div>Logo</div>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className='nav-link' to='/shop'>
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navigation;
