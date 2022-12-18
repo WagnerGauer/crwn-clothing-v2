c1c6097f2bd824584d8cf461477976165e50ec03
Wagner Gauer <81208157+WagnerGauer@users.noreply.github.com>
2022-12-18 17:11:46 -0300

    bring logo to project, add styles to navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,16 @@
 import { Fragment } from "react"; // react forces me to use a top level element, if I don't want to have a div in my html, I have to use this
 import { Outlet, Link } from "react-router-dom";
 
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+
+import './navigation.styles.scss';
+
 const Navigation = () => {
     return (
         <Fragment>
             <div className="navigation">
                 <Link className="logo-container" to="/">
-                    <div>Logo</div>
+                    <CrownLogo className="logo"></CrownLogo>
                 </Link>
                 <div className="nav-links-container">
                     <Link className='nav-link' to='/shop'>
